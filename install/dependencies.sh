a1d7d7c1f425302afffb01a230e51edf33100221
Philip I. Thomas <pthomas@opendns.com>
2015-09-20 14:06:04 -0500

    build fix

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 # Install dependencies for this repo
+sudo apt-get install build-essential --yes --force-yes
+
 julia -e 'Pkg.add("JuMP")'
 julia -e 'Pkg.add("Cbc")'    # Branch and bound solver
 julia -e 'Pkg.add("Ipopt")'  # Nonlinear solver
