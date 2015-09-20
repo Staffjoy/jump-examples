0c9160be272128ac5cf734bb3a6d877a270d03ca
Philip I. Thomas <mail@philipithomas.com>
2015-09-20 15:28:05 -0500

    force install of fortran compiler

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 # Install dependencies for this repo
+sudo add-apt-repository ppa:picaso/octave
+sudo apt-get update ; sudo apt-get install octave
 sudo apt-get install build-essential --yes --force-yes
 
 julia -e 'Pkg.add("JuMP")'
