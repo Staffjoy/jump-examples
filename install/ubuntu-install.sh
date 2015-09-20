714a01293dcd08d74654bbf10acc16ed1a66117c
Philip I. Thomas <pthomas@opendns.com>
2015-09-20 13:50:15 -0500

    try running on new travis infrastructure

--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-sudo apt-get install build-essential --yes --force-yes
-sudo add-apt-repository -y ppa:staticfloat/juliareleases
-sudo add-apt-repository -y ppa:staticfloat/julia-deps
-sudo apt-get update --yes --force-yes
-sudo apt-get install julia --yes --force-yes
+apt-get install build-essential --yes --force-yes
+add-apt-repository -y ppa:staticfloat/juliareleases
+add-apt-repository -y ppa:staticfloat/julia-deps
+apt-get update --yes --force-yes
+apt-get install julia --yes --force-yes
