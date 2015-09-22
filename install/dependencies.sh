43b810335466a1a198edd1b025135c5c17bfec04
Philip I. Thomas <mail@philipithomas.com>
2015-09-22 09:06:04 -0700

    try fixing travis.yml using .travis.yml from JuliaOpt/JuMP.jl

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 # Install dependencies for this repo
-sudo add-apt-repository ppa:picaso/octave
-sudo apt-get update ; sudo apt-get install octave
-sudo apt-get install build-essential --yes --force-yes
 
 julia -e 'Pkg.add("JuMP")'
 julia -e 'Pkg.add("Cbc")'    # Branch and bound solver
